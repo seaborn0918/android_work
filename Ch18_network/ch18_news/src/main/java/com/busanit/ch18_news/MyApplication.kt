a85333c5dc07548fa14ccff9614151b80a7a3687
seaborn0918 <atjhmm@gmail.com>
2023-06-20 17:01:52 +0900

    no message

--- Ch18_network/ch18_news/src/main/java/com/busanit/ch18_news/MyApplication.kt
+++ Ch18_network/ch18_news/src/main/java/com/busanit/ch18_news/MyApplication.kt
@@ -2,6 +2,9 @@ package com.busanit.ch18_news
 
 import android.app.Application
 import android.app.DownloadManager.Query
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 class MyApplication : Application() {
   companion object {
@@ -10,5 +13,11 @@ class MyApplication : Application() {
     val BASE_URL = "https://newsapi.org"
     val USER_AGENT =
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36(KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
+
+    // Retrofit 용 추가
+    val retrofit: Retrofit = Retrofit.Builder()
+      .baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
+    var networkService: INetworkService = retrofit.create(INetworkService::class.java)
+
   }
 }
